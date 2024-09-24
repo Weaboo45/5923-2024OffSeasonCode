4580bfbd95700140121a286149e7727db3f416a1
5923-WILDCATS <ynotsans@gmail.com>
2024-09-24 16:04:02 -0500

    small update

--- src/main/java/frc/robot/commands/autoCommands/PIDButtons.java
+++ src/main/java/frc/robot/commands/autoCommands/PIDButtons.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.autoCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.PIDSubsystems.*;
 import java.util.function.Supplier;
 
