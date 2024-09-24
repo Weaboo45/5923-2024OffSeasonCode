5a6e751ff3bbb3362d83c2595c7242e738828dc8
5923-WILDCATS <ynotsans@gmail.com>
2024-09-24 16:23:25 -0500

    buildconst update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "5923-2024OffSeasonCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "9ce7d70834ed94a6932e6b19d649ff973203bf07";
-  public static final String GIT_DATE = "2024-09-05 04:10:37 GMT";
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "4580bfbd95700140121a286149e7727db3f416a1";
+  public static final String GIT_DATE = "2024-09-24 21:04:02 GMT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-09-10 22:25:17 GMT";
-  public static final long BUILD_UNIX_TIME = 1726007117111L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-09-24 21:05:14 GMT";
+  public static final long BUILD_UNIX_TIME = 1727211914733L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
